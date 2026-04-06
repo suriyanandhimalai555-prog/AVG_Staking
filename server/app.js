f6821dfbd425fcc9e8bc345bcaa3fa9ff62ac144
Muthu4558 <muthukdm45@gmail.com>
2026-04-06 16:18:14 +0530

    done

--- server/app.js
+++ server/app.js
@@ -12,7 +12,12 @@ import "./cron/roiCron.js"
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
